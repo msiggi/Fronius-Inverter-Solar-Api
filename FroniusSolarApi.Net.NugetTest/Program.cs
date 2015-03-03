f541a4443bda793f0933a97f8132abf2ad453a55
Marcel Siegel <marcel.siegel@taetzschwitz.de>
2015-03-03 13:46:07 +0100

    new Json-Serializer

--- FroniusSolarApi.Net.NugetTest/Program.cs
+++ FroniusSolarApi.Net.NugetTest/Program.cs
@@ -10,8 +10,11 @@ namespace FroniusSolarApi.Net.NugetTest
         static void Main(string[] args)
         {
             var values = new FroniusSolarApi.SolarState("192.168.121.90", 1).GetValues();
+
             Console.WriteLine(string.Concat("Day-Energy: ", values.DayEnergy));
+            Console.WriteLine(string.Concat("Total-Energy: ", values.TotalEnergy));
             Console.WriteLine(string.Concat("Current Power: ", values.CurrentPower));
+
             System.Threading.Thread.Sleep(2000);
         }
     }
